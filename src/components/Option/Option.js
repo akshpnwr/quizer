43a2e7483e3d41aa1e90d88230da074e70e79b50
akshpnwr <akashpanwar9976@gmail.com>
2021-07-23 09:46:01 +0530

    redesign the options

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,26 +1,12 @@
-import { useRef } from "react";
 import classes from "./Option.module.css";
 
 const Option = (props) => {
-  const inputRef = useRef();
-
-  const onClickHandler = () => {
-    props.click(inputRef.current);
-  };
-
   return (
-    <div className={classes.input} onClick={onClickHandler}>
-      <input
-        checked={props.checked}
-        type="radio"
-        name="select"
-        ref={inputRef}
-      />
-      <label htmlFor={props.children} className={classes.label}>
-        <div className={classes.dot} />
-        <span className={classes.span}>{props.children}</span>
-      </label>
-    </div>
+    <label htmlFor={props.children} className={classes.label}>
+      <input type="radio" name="select" id={props.children} />
+      <div className={classes.dot} />
+      {props.children}
+    </label>
   );
 };
 export default Option;
